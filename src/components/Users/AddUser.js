fbb4d102dece79672cb7170bee3e6da1a7c7b6de
Mateus Cesar <mateuscesarglima@gmail.com>
2022-03-15 16:22:20 -0300

    Error Handler

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,25 +1,39 @@
 import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
 import Input from "../UI/Input";
 import styles from "./AddUser.module.css";
 
 const AddUser = (props) => {
   const [userName, setUserName] = useState("");
   const [userAge, setUserAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
+    //Não recarrega a página
     event.preventDefault();
+    //Lógica para validação de campos.
+    //a função trim() retira todos os espaços em branco da string.
+    //Se após todos os espaços em branco o tamanho da string for igual a 0
+    // Ou se o tamanho da string idade for igual a 0
+    //A condição entenderá que o campo está vazio e irá retornar.
     if (userName.trim().length === 0 || userAge.trim().length === 0) {
-      return;
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age(non-empty values).",
+      });
     }
 
     if (+userAge < 1) {
-      return;
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age ( > 0)",
+      });
     }
     props.onAddUser(userName, userAge);
-    setUserName('');
-    setUserAge('');
+    setUserName("");
+    setUserAge("");
   };
 
   const userNameChangeHandler = (event) => {
@@ -30,37 +44,49 @@ const AddUser = (props) => {
     setUserAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    //styles.input está sendo passado para o componente card como props para utilização da estilização por lá.
-    <Card className={styles.input}>
-      <form onSubmit={addUserHandler}>
-        <div>
-          <label>Username</label>
-          <Input
-            type="text"
-            name="username"
-            value={userName}
-            id="username"
-            placeholder="Enter your name"
-            onChange={userNameChangeHandler}
-          />
-        </div>
-        <div>
-          <label>Age (Years)</label>
-          <Input
-            type="number"
-            id="age"
-            name="age"
-            value={userAge}
-            placeholder="Enter your age"
-            onChange={ageChangeHandler}
-          />
-        </div>
-        <div>
-          <Button type="submit">Add User</Button>
-        </div>
-      </form>
-    </Card>
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card className={styles.input}>
+        <form onSubmit={addUserHandler}>
+          <div>
+            <label>Username</label>
+            <Input
+              type="text"
+              name="username"
+              value={userName}
+              id="username"
+              placeholder="Enter your name"
+              onChange={userNameChangeHandler}
+            />
+          </div>
+          <div>
+            <label>Age (Years)</label>
+            <Input
+              type="number"
+              id="age"
+              name="age"
+              value={userAge}
+              placeholder="Enter your age"
+              onChange={ageChangeHandler}
+            />
+          </div>
+          <div>
+            <Button type="submit">Add User</Button>
+          </div>
+        </form>
+      </Card>
+    </div>
   );
 };
 
